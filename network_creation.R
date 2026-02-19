16138e668461cc6b672882c70ca9798fadaa5c87
claudiamameli <claudiamameli.work@gmail.com>
2026-02-19 17:19:05 +0100

    Polishing script and cleaning up unnecessary comments

--- network_creation.R
+++ network_creation.R
@@ -20,150 +20,64 @@ library(cluster)
 library(cowplot)
 library(caseconverter) 
 
+# Importing functions 
+source("~/Desktop/Repositories/MEC8_Snowbed_Alpine_Species/functions_script.R")
 
 
-# final dataframes ------------------------------------------------------------
+# Import node dataframes ------------------------------------------------------------
+graphs_path <- "~/Desktop/Repositories/MEC8_Snowbed_Alpine_Species/species_specific_df/"
+graph_df <- list.files(graphs_path, pattern = "buff3.csv$", full.names = TRUE)
+
+graph_df_names <- tools::file_path_sans_ext(basename(graph_df))
+
+
+list2env(setNames(lapply(graph_df, read.csv),
+                  graph_df_names),
+         envir = .GlobalEnv)
+
+
+
+
+# Node Visualisation and Distribution of area values  -------------------------------------------------
 plot_nodes(g_supinum_pres_buff3, "Present", "3")
 plot_nodes(g_supinum_fut245_buff3, "Future 2-4.5", "3")
 plot_nodes(g_supinum_fut585_buff3, "Future 5-8.5", "3")
 
-kernels_target_sp
+g_supinum_buff3 <- bind_rows(
+  g_supinum_pres_buff3 %>% mutate(scenario = "Present", buffer = "3"),
+  g_supinum_fut245_buff3 %>% mutate(scenario = "Future 2–4.5", buffer = "3"),
+  g_supinum_fut585_buff3 %>% mutate(scenario = "Future 5–8.5", buffer = "3")
+)
+
+# Order scenarios
+g_supinum_buff3 <- g_supinum_buff3 %>%
+  mutate(scenario = factor(scenario, levels = c("Present", "Future 2–4.5", "Future 5–8.5")))
 
 
-
-# Distribution of area values  -------------------------------------------------
-ggplot(subset(g_supinum_all, buffer == "3"),
+ggplot(subset(g_supinum_buff3, buffer == "3"),
        aes(x = scenario, y = log(area_m2))) +
   geom_boxplot() +
   theme_bw() +
   labs(
     x = "Scenario",
     y = "Patch area - log(m²)",
-    title = "Patch area distribution - buffer 3m"
+    title = "Patch area distribution"
   )
 
 
-
-# edge to edge function  --------------------------------------------------
-df_patch <- g_supinum_pres_buff3
-# q0.9995
-d <- 50
-
-euclidean_network_e2e <- function(d, df_patch) {
-  # matrix of distances between all pairs of patches
-  mat_dist <- as.matrix(dist(dplyr::select(df_patch, longitude,latitude), method="euclidean", diag=TRUE, upper=TRUE))
-  print('preliminary matrix created')
-  
-  # calculate and extrapolate the radius of individual patches
-  df_patch$radius <- sqrt(df_patch$area_m2 / pi)
-  radius_list <- dplyr::select(df_patch, radius)
-  
-  
-  # creation  of a new distance matrix considering edge to edge distance
-  e2e_mat_dist <- mat_dist
-  e2e_mat_dist[] <- 0 
-  
-  #this takes a long time <<<<<<<<<<<<<<<<<<< Changed into the version below
-  # for (i in 1:nrow(mat_dist)) {
-  #   for (j in 1:ncol(mat_dist)) {
-  #     e2e_mat_dist[i, j] <- mat_dist[i, j] - (radius_list$radius[i] + radius_list$radius[j])
-  #   }
-  # }
-  
-  # this avoids loops, which was taking too long. 
-  radius_matrix <- outer(radius_list$radius, radius_list$radius, `+`)
-  e2e_mat_dist <- mat_dist - radius_matrix
-  print('edge to edge matrix created')
-  
-  # This is to make sure diagonal and negative values are set to 0 
-  diag(e2e_mat_dist) <- 0 
-  # sum(is.na(e2e_mat_dist))
-  # sum(e2e_mat_dist < 0)
-  e2e_mat_dist[e2e_mat_dist < 0] <- 0
-  # sum(e2e_mat_dist == 0.1)
-  
-  # adjacency matrix - 1 = link between patch i and j; 0 = no link between patch i and j
-  # assign 1 if distance between patch i and j is less or equal to threshold distance
-  # assign 0 if distance between patch i and j is greater than threshold distance
-  m_adj <- e2e_mat_dist
-  m_adj[e2e_mat_dist <= d] <- 1
-  m_adj[e2e_mat_dist > d] <- 0
-  diag(m_adj) <- 0 # set diagonal to 0
-  # sum(m_adj == 1)
-  # sum(m_adj == 0)
-  # sum(diag(m_adj))
-  print('Matrix cleaned up')
-  
-  # Connectance (number of realised links / number of all possible links)
-  n = nrow(m_adj) # number of patches
-  c = sum(m_adj) / (n*(n-1))
-  
-  
-  # create dataframe with connections between patches
-  edge_index <- which(m_adj==1, arr.ind=TRUE)
-  patch_names <- df_patch$patch
-  edges <- data.frame(from = patch_names[edge_index[, "row"]],
-                      to = patch_names[edge_index[, "col"]]) %>%
-    rowwise() %>%
-    mutate(a = min(from, to),
-           b = max(from, to)) %>%
-    ungroup() %>%
-    dplyr::select(from = a, to = b) %>%
-    distinct()
-  
-  # rename site to name (for igraph)
-  nodes <- df_patch %>% rename(name = patch)
-  
-  # create igraph object
-  g <- graph_from_data_frame(d=edges, vertices=nodes, directed=FALSE)
-  
-  
-  # Modularity
-  modules <- cluster_infomap(g) #switch to different function if needed
-  m <- modularity(modules)
-  
-  # Centrality metrics
-  c_betwenness <- betweenness(g, directed=FALSE)
-  c_closeness <- closeness(g)
-  # c_bridge <- bridge(g, communities = modules) #takes a long time <<<<<<<<<
-  deg_list <- degree(g)
-  
-  # Compute components
-  comp <- components(g, mode = "weak")  # Use mode = "strong" for directed Strongly Connected Component (SCC)
-  largest_size <- max(comp$csize)  
-  total_nodes <- vcount(g)   
-  fraction <- largest_size / total_nodes  # Fraction of nodes in largest component
-  
-  # Calculate percentage of nodes connected
-  connected_nodes <- sum(deg_list >= 1)
-  percent_connected <- (connected_nodes / nrow(m_adj)) * 100
-  
-  return(list(
-    edge_index = edge_index,
-    distance = d, 
-    connectance = c, 
-    modularity = m,
-    betweenness = c_betwenness,
-    closeness = c_closeness,
-    #bridge = c_bridge,
-    degree = deg_list,
-    comp_number = comp$no,
-    largest_comp = largest_size,
-    fraction_comp = fraction,
-    percent_connected = percent_connected,
-    graph = g
-  ))
-}
+# Network creation & Metric calculation  --------------------------------------------------
+# See "functions_script" for full function
 
 
+# G. supinum networks ------------------------------------------------------
+gnap_pres_net <- euclidean_network_e2e(50, g_supinum_pres_buff3)  
+gnap_fut245_net <- euclidean_network_e2e(50, g_supinum_fut245_buff3)  
+gnap_fut585_net <- euclidean_network_e2e(50, g_supinum_fut585_buff3)  
 
-trial_net <- euclidean_network_e2e(50, g_supinum_pres_buff3)  
-trial_net_fut <- euclidean_network_e2e(50, g_supinum_fut245_buff3)  
 
-
-
-trial_net$percent_connected
-trial_net_fut$percent_connected
+gnap_pres_net$percent_connected
+gnap_fut245_net$percent_connected
+gnap_fut585_net$percent_connected
 
 
 g <- trial_net$graph

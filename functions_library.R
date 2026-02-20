ba9396687d8d034df3a5f126b06f65adab6f4642
claudiamameli <claudiamameli.work@gmail.com>
2026-02-20 14:33:26 +0100

    Changed file name and added new functions

--- functions_library.R
+++ functions_library.R
@@ -1,3 +1,30 @@
+# Library -----------------------------------------------------------------
+library(tidyverse)
+library(sp)
+library(colorRamps)
+library(RColorBrewer)
+library(paletteer)
+library(terra)
+library(sf)
+library(rworldmap)
+library(gpkg)
+library(raster)
+library(igraph)
+library(ggplot2)
+library(ggridges)
+library(readxl)
+library(patchwork)
+library(networktools)
+library(cluster)
+library(cowplot)
+library(caseconverter) 
+library(mgcv)
+library(gam)
+
+
+
+
+# Functions ---------------------------------------------------------------
 # Function for plotting nodes
 plot_nodes <- function(df, scenario, buffer) {
   ggplot(df, aes(x = longitude, y = latitude)) +
@@ -74,7 +101,6 @@ euclidean_network_e2e <- function(d, df_patch) {
   # Centrality metrics
   c_betwenness <- betweenness(g, directed=FALSE)
   c_closeness <- closeness(g)
-  # c_bridge <- bridge(g, communities = modules) #takes a long time <<<<<<<<<
   deg_list <- degree(g)
   
   # Compute components
@@ -98,7 +124,6 @@ euclidean_network_e2e <- function(d, df_patch) {
     modularity = m,
     betweenness = c_betwenness,
     closeness = c_closeness,
-    #bridge = c_bridge,
     degree = deg_list,
     comp_number = comp$no,
     largest_comp = largest_size,
@@ -106,4 +131,78 @@ euclidean_network_e2e <- function(d, df_patch) {
     percent_connected = percent_connected,
     graph = g
   ))
+}
+
+#' Function for plotting graph 
+#' g --> graph from the euclidean_network_e2e function
+#' d --> distance used for the network creation
+graph_plot_fun <- function(g, d, title){ 
+  nodes <- igraph::as_data_frame(g, what = "vertices")
+
+  layout_coords <- nodes %>%
+    dplyr::select(name, longitude, latitude) %>%
+    filter(name %in% V(g)$name) %>%
+    arrange(match(name, V(g)$name)) %>%
+    dplyr::select(longitude, latitude) %>% 
+    as.matrix()
+  plot(g,
+       layout = layout_coords,
+       vertex.size = log10(nodes$area_m2),
+       #vertex.label = V(g)$name,
+       vertex.label = "",
+       vertex.color = "black",
+       edge.color = "red",
+       edge.width = 2,
+       main = paste0(title, " Spatial Network, d = ",d))
+}
+
+# Functions to create a full dataframe with the results from the euclidean_network_e2e function
+net_metrics_fun <-  function(full_results, scenario_){
+  data.frame(
+  distance = full_results$distance,
+  percent_connected = full_results$percent_connected,
+  connectance = full_results$connectance,
+  modularity = full_results$modularity,
+  comp_number = full_results$comp_number, 
+  comp_largest = full_results$largest_comp,
+  comp_fraction = full_results$fraction_comp,
+  scenario = scenario_)
+}
+
+node_metrics_fun <- function(full_results, original_node_df){
+  df <- data.frame(
+    patch = names(full_results$betweenness),
+    betweenness = full_results$betweenness,
+    closeness = full_results$closeness,
+    degree = full_results$degree
+  )
+  
+  # Change NaN --> 0 
+  numeric_cols <- sapply(df, is.numeric)
+  df[numeric_cols] <- lapply(df[numeric_cols], function(col) {
+    col[is.nan(col)] <- 0
+    return(col)
+  })
+  
+  # Adding node areas and ranking nodes by connectivity measures
+  df <- df %>%
+    mutate(area_m2 = original_node_df$area_m2) %>% 
+    mutate(
+      betweenness_rank = ifelse( betweenness != 0, rank(-betweenness, ties.method = "min"), NA),
+      closeness_rank = ifelse(closeness != 0 & closeness != 1, rank(-closeness, ties.method = "min"), NA),
+      degree_rank = ifelse(degree != 0, rank(-degree, ties.method = "min"), NA),
+      area_rank = rank(-area_m2)
+    )
+  return(df)
+}
+
+# Modularity analysis function 
+random_mod_cal_fun <- function(g, num_graphs) {
+  random_modularity <- numeric(num_graphs)
+  for (i in 1:num_graphs) {
+    g_rand <- sample_degseq(degree(g), method = "configuration")
+    mod_rand <- cluster_infomap(g_rand) #can be changed to other clustering functions - for comparison better to match with same as in euclidean_network_e2e function
+    random_modularity[i] <- modularity(mod_rand)
+  }
+  return(random_modularity)
 }

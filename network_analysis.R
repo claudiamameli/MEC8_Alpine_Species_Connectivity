c346ccc6b676ec62a8ee910ed1ba3af7bf79ba4b
claudiamameli <claudiamameli.work@gmail.com>
2026-03-04 15:51:18 +0100

    Degree distribution analysis for future scenarios and table creation

--- network_analysis.R
+++ network_analysis.R
@@ -15,7 +15,7 @@ read_file_fun("graph_files/", readRDS)
 pres_random_mod <- random_mod_cal_fun(gnap_pres_net$graph, 1000)
 pres_mod <- gnap_net_metrics$modularity[gnap_net_metrics$scenario == "pres"]
 
-hist_rand_degree(pres_random_mod, pres_mod, 10, "Present")
+hist_rand_mod(pres_random_mod, pres_mod, 10, "Present")
 
 # Calculate Z-score
 pres_z_score <- mod_z_score(pres_random_mod, pres_mod)
@@ -28,7 +28,7 @@ graph_plot_fun(gnap_pres_net$graph, 50, "Present", gnap_pres_net$membership)
 fut245_random_mod <- random_mod_cal_fun(gnap_ssp245_net$graph, 1000)
 fut245_mod <- gnap_net_metrics$modularity[gnap_net_metrics$scenario == "ssp245"]
 
-hist_rand_degree(fut245_random_mod, fut245_mod, 10, "ssp245")
+hist_rand_mod(fut245_random_mod, fut245_mod, 10, "ssp245")
 
 # Calculate Z-score
 fut245_z_score <- mod_z_score(fut245_random_mod, fut245_mod)
@@ -37,11 +37,11 @@ print(paste("Fut 245 Z-score (Degree controlled):", round(fut245_z_score, 3)))
 # Visualise memberships
 graph_plot_fun(gnap_ssp245_net$graph, 50, "ssp245", gnap_ssp245_net$membership)
 
-### b. Future ssp585 -----------------------------------------------------------
+### c. Future ssp585 -----------------------------------------------------------
 fut585_random_mod <- random_mod_cal_fun(gnap_ssp585_net$graph, 1000)
 fut585_mod <- gnap_net_metrics$modularity[gnap_net_metrics$scenario == "ssp585"]
 
-hist_rand_degree(fut585_random_mod, fut585_mod, 10, "ssp585")
+hist_rand_mod(fut585_random_mod, fut585_mod, 10, "ssp585")
 
 # Calculate Z-score
 fut585_z_score <- mod_z_score(fut585_random_mod, fut585_mod)
@@ -49,6 +49,24 @@ print(paste("Fut 585 Z-score (Degree controlled):", round(fut585_z_score, 3)))
 
 # Visualise memberships
 graph_plot_fun(gnap_ssp585_net$graph, 50, "ssp585", gnap_ssp585_net$membership)
+
+### >>> Table -----------------------------------------------------------
+modularity_summary <- data.frame(
+  scenario = c("Present", "ssp2-4.5", "ssp5-8.5"),
+  nodes      = c(vcount(gnap_pres_net$graph),
+                   vcount(gnap_ssp245_net$graph),
+                   vcount(gnap_ssp585_net$graph)),
+  edges      = c(ecount(gnap_pres_net$graph),
+                   ecount(gnap_ssp245_net$graph),
+                   ecount(gnap_ssp585_net$graph)),
+  modules    = c(length(unique(gnap_pres_net$membership)),
+                   length(unique(gnap_ssp245_net$membership)),
+                   length(unique(gnap_ssp585_net$membership))),
+  modularity = round(c(pres_mod, fut245_mod, fut585_mod), 4),
+  z_score = round(c(pres_z_score, fut245_z_score, fut585_z_score),4)
+)
+
+modularity_summary
 
 ## 3.2 Degree distribution ------------------------------------
 # This section aims to understand which degree distribution our network has to classify if the networks are closer to a random, scale-free or small-world network.
@@ -59,20 +77,12 @@ g_gnap_pres <- gnap_pres_net$graph
 
 vcount(g_gnap_pres)
 ecount(g_gnap_pres)
-transitivity(g_gnap_pres) # clustering coefficient
-transitivity(g_gnap_pres, "local")  # Per-patch
-mean(transitivity(g_gnap_pres, "local"), na.rm=TRUE)
-
+gnap_pres_net$global_trans
 components(g_gnap_pres)
 mean(igraph::degree(g_gnap_pres))
 
-hist(igraph::degree(g_gnap_pres), xlim = c(0, max(igraph::degree(g_gnap_pres))), breaks = 100)
-text(x = max(igraph::degree(g_gnap_pres)),
-     y = 1,
-     labels = "*",
-     col = "black",
-     cex = 4)
-
+# Visualisation
+hist_degree(g_gnap_pres, "Present Original Network")
 
 #### > Random network ----------------------------------------------------------
 random_net <- sample_correlated_gnp(g_gnap_pres, p = edge_density(g_gnap_pres), corr = 0.6)
@@ -83,27 +93,11 @@ transitivity(random_net)
 components(random_net)
 mean(igraph::degree(random_net))
 
-
-
-#Visualisation 
-hist(igraph::degree(random_net), xlim = c(0, max(igraph::degree(random_net))), breaks = 100)
-abline(v = mean(igraph::degree(g_gnap_pres)), col = "#FF4000", lwd = 2, lty = 2)
-text(x = 45,
-     y = par("usr")[4] * 0.9,
-     labels = paste("Original Network \n Mean = ", round(mean(igraph::degree(g_gnap_pres)), 2)),
-     col = "#FF4000",
-     cex = 1)
-text(x = max(igraph::degree(random_net)),
-     y = 1,
-     labels = "*",
-     col = "black",
-     cex = 4)
-
+# Visualisation 
+hist_degree(random_net, "Present Random Network")
 
 #### > Scale-free network ------------------------------------------------------
 m_estimate <- round(ecount(g_gnap_pres) / vcount(g_gnap_pres))
-
-
 scalefree_net <- sample_pa(n = vcount(g_gnap_pres), m = m_estimate, directed = FALSE, zero.appeal = 0)
 
 vcount(scalefree_net)
@@ -113,18 +107,108 @@ components(scalefree_net)
 mean(igraph::degree(scalefree_net))
 
 #Visualisation 
+hist_degree(scalefree_net, "Present Scale-free Network")
 
-hist(igraph::degree(scalefree_net), xlim = c(0, max(igraph::degree(scalefree_net))), breaks = 100)
-abline(v = mean(igraph::degree(g_gnap_pres)), col = "#FF4000", lwd = 2, lty = 2)
-text(x = 20,
-     y = par("usr")[4] * 0.9,
-     labels = paste0("Original Network \n Mean = ", round(mean(igraph::degree(g_gnap_pres)), 2)),
-     col = "#FF4000",
-     cex = 1)
-text(x = max(igraph::degree(scalefree_net)),
-     y = 1,
-     labels = "*",
-     col = "black",
-     cex = 4)
+### b. Future ssp245 -----------------------------------------------------------------
+#### > Original network --------------------------------------------------------
+g_gnap_ssp245 <- gnap_ssp245_net$graph
 
-# Create table with transitivity etc <<<<<< 
+vcount(g_gnap_ssp245)
+ecount(g_gnap_ssp245)
+gnap_ssp245_net$global_trans
+components(g_gnap_ssp245)
+mean(igraph::degree(g_gnap_ssp245))
+
+# Visualisation
+hist_degree(g_gnap_ssp245, "ssp245 Original Network")
+
+#### > Random network ----------------------------------------------------------
+random_net_ssp245 <- sample_correlated_gnp(g_gnap_ssp245, p = edge_density(g_gnap_ssp245), corr = 0.6)
+
+vcount(random_net_ssp245)
+ecount(random_net_ssp245)
+transitivity(random_net_ssp245) 
+components(random_net_ssp245)
+mean(igraph::degree(random_net_ssp245))
+
+# Visualisation 
+hist_degree(random_net_ssp245, "ssp245 Random Network")
+
+#### > Scale-free network ------------------------------------------------------
+m_estimate <- round(ecount(g_gnap_ssp245) / vcount(g_gnap_ssp245))
+scalefree_net_ssp245 <- sample_pa(n = vcount(g_gnap_ssp245), m = m_estimate, directed = FALSE, zero.appeal = 0)
+
+vcount(scalefree_net_ssp245)
+ecount(scalefree_net_ssp245)
+transitivity(scalefree_net_ssp245) 
+components(scalefree_net_ssp245)
+mean(igraph::degree(scalefree_net_ssp245))
+
+#Visualisation 
+hist_degree(scalefree_net_ssp245, "ssp245 Scale-free Network")
+
+### c. Future ssp585 -----------------------------------------------------------
+#### > Original network --------------------------------------------------------
+g_gnap_ssp585 <- gnap_ssp585_net$graph
+
+vcount(g_gnap_ssp585)
+ecount(g_gnap_ssp585)
+gnap_ssp585_net$global_trans
+components(g_gnap_ssp585)
+mean(igraph::degree(g_gnap_ssp585))
+
+# Visualisation
+hist_degree(g_gnap_ssp585, "ssp585 Original Network")
+
+#### > Random network ----------------------------------------------------------
+random_net_ssp585 <- sample_correlated_gnp(g_gnap_ssp585, p = edge_density(g_gnap_ssp585), corr = 0.6)
+
+vcount(random_net_ssp585)
+ecount(random_net_ssp585)
+transitivity(random_net_ssp585) 
+components(random_net_ssp585)
+mean(igraph::degree(random_net_ssp585))
+
+# Visualisation 
+hist_degree(random_net_ssp585, "ssp585 Random Network")
+
+#### > Scale-free network ------------------------------------------------------
+m_estimate <- round(ecount(g_gnap_ssp585) / vcount(g_gnap_ssp585))
+scalefree_net_ssp585 <- sample_pa(n = vcount(g_gnap_ssp585), m = m_estimate, directed = FALSE, zero.appeal = 0)
+
+vcount(scalefree_net_ssp585)
+ecount(scalefree_net_ssp585)
+transitivity(scalefree_net_ssp585) 
+components(scalefree_net_ssp585)
+mean(igraph::degree(scalefree_net_ssp585))
+
+#Visualisation 
+hist_degree(scalefree_net_ssp585, "ssp585 Scale-free Network")
+
+### >>> Table ---------------------------------------------------------------
+degree_summary <- data.frame(
+  scenario = rep(c("Present", "ssp2-4.5", "ssp5-8.5"), each = 3),
+  network_type = rep(c("Original", "Random", "Scale-free"), 3),
+  transitivity = c(gnap_pres_net$global_trans, 
+                   transitivity(random_net), 
+                   transitivity(scalefree_net),
+                   gnap_ssp245_net$global_trans, 
+                   transitivity(random_net_ssp245), 
+                   transitivity(scalefree_net_ssp245),
+                   gnap_ssp585_net$global_trans, 
+                   transitivity(random_net_ssp585), 
+                   transitivity(scalefree_net_ssp585)),
+  mean_degree = c(round(mean(igraph::degree(g_gnap_pres)), 2), 
+                  round(mean(igraph::degree(random_net)), 2), 
+                  round(mean(igraph::degree(scalefree_net)), 2),
+                  round(mean(igraph::degree(g_gnap_ssp245)), 2), 
+                  round(mean(igraph::degree(random_net_ssp245)), 2), 
+                  round(mean(igraph::degree(scalefree_net_ssp245)), 2),
+                  round(mean(igraph::degree(g_gnap_ssp585)), 2),
+                  round(mean(igraph::degree(random_net_ssp585)), 2), 
+                  round(mean(igraph::degree(scalefree_net_ssp585)), 2)))
+
+degree_summary
+
+
+
